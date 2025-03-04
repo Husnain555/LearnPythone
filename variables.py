64f9581d1dfff3737067cb9a28803c31e16b222c
Husnain555 <husnain.babar.dev@gmail.com>
2025-03-04 15:35:03 +0500

    make directory path

---
+++ variables.py
@@ -0,0 +1,22 @@
+# integer
+from operator import truediv
+
+a = 1
+b = 2
+print(a+b)
+# its goves us a integeer
+
+# floting point numbers
+c = 70.265
+print(c)
+
+# string
+d = "hello i'm learning python"
+print(d)
+
+e = True
+print(e)
+
+
+f = None
+print(f)
