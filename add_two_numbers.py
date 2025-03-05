e83e3f32c9b9524f85fc71cfae72550f6da6573b
Husnain555 <husnain.babar.dev@gmail.com>
2025-03-05 06:40:15 +0500

    convertion and exercises

---
+++ add_two_numbers.py
@@ -0,0 +1,4 @@
+def add(a,b):
+    return a+b
+number = add(4,8)
+print(number)
---
+++ type_functions.py
@@ -0,0 +1,12 @@
+a = 3
+b = '3'
+c = 31.2
+d = None
+e = not True
+print(type(a),type(b),type(c),type(d),type(e))
+
+# convertions
+aa = str(a)
+bb = float(b)
+cc = str(c)
+print(type(aa),type(bb),type(cc))
