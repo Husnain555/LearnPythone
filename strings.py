f8be1da2c0c21bbdf427d2841ce7314e0e91a3ff
Husnain555 <husnain.babar.dev@gmail.com>
2025-03-07 06:19:31 +0500

    convertion and exercises

---
+++ strings.py
@@ -0,0 +1,13 @@
+name = 'husnain'
+print(name[0:3])
+print(name[-3:])
+print(name[:3])
+
+print(len(name))
+print(name.endswith("n"))
+print(name.startswith("H"))
+name2 = name.capitalize()
+print(name2)
+
+greetings = "   Hello how are you \n hopefully you are fine"
+print(greetings)
