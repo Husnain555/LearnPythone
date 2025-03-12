03153e4894127a31dc94753a277b561e7ee20b4f
Husnain555 <husnain.babar.dev@gmail.com>
2025-03-12 14:27:52 +0500

    if else

--- conditions.py
+++ conditions.py
@@ -1,5 +1,6 @@
 from typing import final
 
+
 a = int(input("Please enter your age:"))
 if a >= 18:
     print("You are young.")
@@ -8,3 +9,10 @@ elif a < 0 :
 else:
     print("You are older.")
 
+# find the greetest number thet user give in in put
+
+numbers = [int(input(f"please enter the the number{i+1}: ")) for i in range(4)]
+greatest = max(numbers)
+if numbers.count(greatest) == len(numbers):
+    print("All number are equal")
+else:print("The greatest number is",greatest)
