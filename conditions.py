d1c8241287b2889af4f667e7f19416717771a168
Husnain555 <husnain.babar.dev@gmail.com>
2025-03-12 14:12:27 +0500

    if else

---
+++ conditions.py
@@ -0,0 +1,10 @@
+from typing import final
+
+a = int(input("Please enter your age:"))
+if a >= 18:
+    print("You are young.")
+elif a < 0 :
+        print("You are  entering an invalid age .")
+else:
+    print("You are older.")
+
