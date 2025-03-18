1ddacd2825af358c07633bde50b894707fdc3709
Husnain555 <husnain.babar.dev@gmail.com>
2025-03-18 15:21:26 +0500

    loops

---
+++ loops.py
@@ -0,0 +1,18 @@
+# for i in range(1,10):
+#     print(i)
+
+# i = 0
+# while i < 11:
+#     print(f"{i}Husnain")
+#     i += 1
+
+my_list = [1, "hello", 3.14, True, [2, 4, 6], {"name": "Husnain"}]
+i = 0
+while i < len(my_list):
+     print(my_list[i])
+     i = i + 1
+
+
+name = "Husnain"
+for letter in name:
+    print(letter)
---
+++ triangular_numbers.py
@@ -0,0 +1,3 @@
+def triangular_number(rows):
+    return rows*(rows+1)//2
+print(triangular_number(rows=3))
