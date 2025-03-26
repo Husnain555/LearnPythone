3ecbc49ee0ce7ba3a5c1a6bd10d37057df61109c
Husnain555 <husnain.babar.dev@gmail.com>
2025-03-26 15:30:29 +0500

    File  I/O

--- file i_O.py
+++ file i_O.py
@@ -1,26 +1,34 @@
-f = open("/home/southville/cvtindell coredirection.txt","rb")
-read= f.read()
-# print(read)
-# print(data)
-f.close()
+# f = open("/home/southville/cvtindell coredirection.txt","rb")
+# read= f.read()
+# # print(read)
+# # print(data)
+# f.close()
+#
+#
+# # how to write a file
+# f = open("/home/southville/cvtindell coredirection.txt","w")
+# st = "How are you buddy "
+# f.write(st)
+# f.close()
+#
+# f = open("/home/southville/cvtindell coredirection.txt")
+# data3 = f.read()
+# print(data3)
+#
+#
+# f = open("/home/southville/cvtindell coredirection.txt","w")
+# f.write("/nThis is addition")
+# f.close()
+#
+#
+# f = open("/home/southville/cvtindell coredirection.txt")
+# data4 = f.read()
+# print(data4)
 
 
-# how to write a file
-f = open("/home/southville/cvtindell coredirection.txt","w")
-st = "How are you buddy "
-f.write(st)
-f.close()
+with  open("/home/southville/cvtindell coredirection.txt") as f:
 
-f = open("/home/southville/cvtindell coredirection.txt")
-data3 = f.read()
-print(data3)
+    print(f.read())
 
 
-f = open("/home/southville/cvtindell coredirection.txt","w")
-f.write("/nThis is addition")
-f.close()
-
-
-f = open("/home/southville/cvtindell coredirection.txt")
-data4 = f.read()
-print(data4)
+# now we dont need to close the file ?
